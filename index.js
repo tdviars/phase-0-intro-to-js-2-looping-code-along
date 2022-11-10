db712196f79415efadf5644f9e705b1033d4bc23
taylor <taylor.viars@yahoo.com>
2022-11-10 17:40:37 -0500

    commit tree f0f5d33df129915aef81de3e2394da9ef8455b0f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(nameArray, event) {
+    let cards = [];
+    for (let i = 0; i < nameArray.length; i++) {
+      cards.push(`Thank you, ${nameArray[i]}, for the wonderful surprise gift!`);
+    }
+    return cards;
+  }
+  
+   function countDown( countdown ) {
+      while ( countdown > 0 ) {
+        console.log( countdown );
+        countdown -= 1;
+      }
+      console.log( countdown );
+    }
